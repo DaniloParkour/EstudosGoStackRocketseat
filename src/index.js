cf5ff7b1a716eaf9d4ea8eea0d2db7f94a8e2989
DaniloParkour <danilomlcarvalho@gmail.com>
2020-04-07 11:03:40 -0300

    Part 2 HTTP methods handling an array

--- src/index.js
+++ src/index.js
@@ -1,55 +1,69 @@
 const express = require('express');
+const {uuid} = require('uuidv4');
 
 //Aplicação criada
 const app = express();
+
+const projects = [];
 
 app.use(express.json());
 
 //Um GET para o recurso (resource) chamado "projects"
 app.get('/projects', (req, res) => {
 
-  const {title, owner} = req.query;
+  const {title} = req.query;
 
-  console.log(title);
-  console.log(owner);
+  const result = title ? projects.filter(proj => proj.title.includes(title))
+                       : projects;
+  
+  return res.json(result);
 
-  return res.json({message: 'Hello World!'});
 });
 
 //Um POST para o recurso (resource) chamado "projects"
 app.post('/projects', (req, res) => {
 
-  const body = req.body;
+  const {title, owner} = req.body;
+  
+  const project = {id: uuid(), title, owner}
+  
+  projects.push(project);
+  
+  return res.json(project);
 
-  console.log(body);
-
-  return res.json([
-    'Projeto 1',
-    'Projeto 2',
-    'Projeto 3',
-  ])
 });
 
 //:id quer dizer que quando for dado um PUT em "projects", ele deverá vir com o informação do id
 //no caso a URL ficaria: //localhost:3333/projects/2  => Para id == 2
 app.put('/projects/:id', (req, res) => {
 
-  const params = req.params;
+  const {id} = req.params;
+  const {title, owner} = req.body;
 
-  console.log(params);
+  const projIndex = projects.findIndex(project => project.id === id);
 
-  return res.json([
-    'Projeto 1',
-    'Projeto 2',
-    'Projeto 4',
-  ]);
+  if(projIndex < 0)
+    return res.status(400).json({error: 'Project Not Found.'});
+
+  const proj = {id, title, owner};
+  projects[projIndex] = proj;
+  
+  return res.json(proj);
 });
 
 app.delete('/projects/:id', (req, res) => {
-  return res.json([
-    'Projeto 1',
-    'Projeto 4'
-  ]);
+
+  const {id} = req.params;
+
+  const projIndex = projects.findIndex(project => project.id === id);
+
+  if(projIndex < 0)
+    return res.status(400).json({error: 'Project Not Found.'});
+
+  projects.splice(projIndex, 1);
+
+
+  return res.status(204).send();
 });
 
 //Dizer em qual porta a aplicação rodará
