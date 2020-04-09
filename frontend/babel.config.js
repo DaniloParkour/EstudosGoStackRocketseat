5e65337f718b42225eb08761113e38589210b22d
DaniloParkour <danilomlcarvalho@gmail.com>
2020-04-09 11:20:47 -0300

    Frontend interacts to backend

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -2,5 +2,8 @@ module.exports = {
 	presets: [ //Produtos de terceiros
 		'@babel/preset-env', //Converte o JS novo para JS antigo (navegador, node, ...)
 		'@babel/preset-react' //Adiciona as funcionalidades do React na conversão
+	],
+	plugins: [
+		'@babel/plugin-transform-runtime'
 	]
 };
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Header from './components/Header';
+import api from './services/api';
+
 import myNameImage from './assets/cat.png';
 
 //O Webpack vai se encarregar da importação do CSS abaixo
@@ -7,10 +9,30 @@ import './App.css';
 
 export default function App() {
 
-  const [projects, setProjects] = useState(['Desenvolvimento Web', 'Front-End Web Com ReactJS']);
+  const [projects, setProjects] = useState([]);
 
-  function handleAddProject() {
-    setProjects([...projects, `Novo Projeto ${Date.now()}`]);
+  //Recebe 2 parametros, a função a disparar e quando disparar
+  //useEffect(() => {}, [projects]); //Será chamada qunado o valor de projects mudar
+  //OBS: A função passada para o useEffect NÃO PODE SER ASYNC
+  useEffect(() => {
+    //A linhan abaixo poderia usar também Async Await no lugar de then
+    api.get('/projects').then( response => { //Executa o then depois que o get retornar a resposta
+      setProjects(response.data);
+    });
+  }, []); //Será chamado quando a página for carregada
+
+  async function handleAddProject() {
+    //setProjects([...projects, `Novo Projeto ${Date.now()}`]);
+
+    const response = await api.post('/projects', {
+      title: `Novo Projeto ${Date.now()}`,
+      owner: 'Danilo Carvalho'
+    });
+
+    const project = response.data;
+
+    setProjects([...projects, project]);
+
   }
 
   return(
@@ -20,7 +42,7 @@ export default function App() {
       <img width={260} src={myNameImage} />
 
       <ul>
-        { projects.map((project => (<li key={project}> {project} </li>))) }
+        { projects.map((project => (<li key={project.id}> {project.title} </li>))) }
       </ul>
 
       <button type="button" onClick={handleAddProject}> Adicionar Novo Projeto! </button>
